215724f799efe48cb67f96e8bb25001c3d162b36
Elizabeth <elizabethnich19@gmail.com>
2017-04-26 11:02:34 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 const app = "I don't do much."
+
+const products = [
+  { name: 'Head & Shoulders Shampoo', price: 4.99, discount: .6 },
+  { name: 'Twinkies', price: 7.99, discount: .45 },
+  { name: 'Oreos', price: 6.49, discount: .8 },
+  { name: 'Jasmine-scented bath pearls', price: 13.99, discount: .7 },
+];
+
+function getTotalAmountForProducts(products) {
+  let totalPrice = 0;
+
+  products.forEach(product => {
+    if(product.discount >= .5) {
+      totalPrice += product.price;
+    }
+  });
+  return totalPrice;
+}
+console.log((getTotalAmountForProducts(products)));
+
+
+console.log(couponLocations.reduce(couponCounter, 0)); 
